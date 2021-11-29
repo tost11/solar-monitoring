1135481979c27fbae8cb19d71f3ebfab7953e0be
Lukas Hagenauer <l.hagenauer@micromata.de>
2021-11-29 09:39:29 +0100

    added backend and env copose file

---
+++ backend/src/main/java/de/tostsoft/solarmonitoring/SolarmonitoringApplication.java
@@ -0,0 +1,13 @@
+package de.tostsoft.solarmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarmonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarmonitoringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/tostsoft/solarmonitoring/SolarmonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tostsoft.solarmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarmonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
