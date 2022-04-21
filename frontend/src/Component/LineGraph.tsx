5f1ac48ba6a28a86b1f0f6d0f73b65e6bc287a13
Lukas Hagenauer <l.hagenauer@micromata.de>
2022-04-21 20:50:45 +0200

    fixed detail view vor moile devices

--- frontend/src/Component/LineGraph.tsx
+++ frontend/src/Component/LineGraph.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
+import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 import moment from "moment";
 import {GraphDataObject} from "./DetailDashboard";
 import {TimeAndDuration} from "../context/time/TimeAndDateSelector";
@@ -21,26 +21,28 @@ export default function LineGraph({timeRange,graphData,unit,labels,min,max,legen
 
   return <div>
     {graphData&&
-      <LineChart className={"Graph"} width={450} height={200} data={graphData.data}
-                 margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-        <CartesianGrid strokeDasharray="3 3"/>
-        <XAxis dataKey="time"
-               domain={[timeRange.start.getTime(), timeRange.end.getTime()]}
-               type='number'
-               tickFormatter={(unixTime) => moment(unixTime).format('HH:mm')}/>
-        <YAxis
-            tickFormatter={value => formatDefaultValueWithUnit(value,unit)}
-            //unit={unit?unit:undefined}
-            domain={[min != undefined ? min : 'dataMin' , max != undefined ? max : 'dataMax' ]}
-        />
-        <Tooltip formatter={(value: number) => formatDefaultValueWithUnit(value,unit)} labelFormatter={(unixTime) => moment(unixTime).format('yyyy-MM-DD HH:mm')}/>
-        {legendOverrideValue ?
-          <Legend content={<div>{legendOverrideValue}</div>}/>:
-          <Legend/>}
-        {labels.map((l,index)=>{
-          return <Line connectNulls={timeRange.duration < 1000 * 60 * 11} dot={false} key={index} type="monotone" dataKey={l} stroke={deviceColours?deviceColours[index]:getGraphColourByIndex(index)}/>
-        })}
-      </LineChart>
+      <ResponsiveContainer width="95%" height={200}>
+        <LineChart className={"Graph"} data={graphData.data}
+                   margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <XAxis dataKey="time"
+                 domain={[timeRange.start.getTime(), timeRange.end.getTime()]}
+                 type='number'
+                 tickFormatter={(unixTime) => moment(unixTime).format('HH:mm')}/>
+          <YAxis
+              tickFormatter={value => formatDefaultValueWithUnit(value,unit)}
+              //unit={unit?unit:undefined}
+              domain={[min != undefined ? min : 'dataMin' , max != undefined ? max : 'dataMax' ]}
+          />
+          <Tooltip formatter={(value: number) => formatDefaultValueWithUnit(value,unit)} labelFormatter={(unixTime) => moment(unixTime).format('yyyy-MM-DD HH:mm')}/>
+          {legendOverrideValue ?
+            <Legend content={<div>{legendOverrideValue}</div>}/>:
+            <Legend/>}
+          {labels.map((l,index)=>{
+            return <Line connectNulls={timeRange.duration < 1000 * 60 * 11} dot={false} key={index} type="monotone" dataKey={l} stroke={deviceColours?deviceColours[index]:getGraphColourByIndex(index)}/>
+          })}
+        </LineChart>
+      </ResponsiveContainer>
     }
   </div>
 
