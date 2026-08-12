63c75672ab4b9734cf232e464da82d4c90fe6069
lukas <tost>
2026-08-12 16:57:38 +0200

    fixed fronend build error

--- frontend/src/views/TagAggregationView.tsx
+++ frontend/src/views/TagAggregationView.tsx
@@ -1,4 +1,4 @@
-npmimport React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   apiGetTagAggregation,
